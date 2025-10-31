af10fe31659c8ff00868e7a25344a41aeef41915
toilaminhbui <toilaminhbui@gmail.com>
2025-10-31 20:59:50 +0700

    add router product, addon gr opt, voucher

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 const app = express();
 
 const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
+const addonGroupRoutes = require("./routes/addonGroupRoutes");
+const addonOptionRoutes = require("./routes/addonOptionRoutes");
+const voucherRoutes = require("./routes/voucherRoutes");
 
 // Middleware
 app.use(cors());
@@ -11,6 +15,10 @@ app.use("/uploads", express.static("uploads"));
 
 // Routes
 app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/addon-groups", addonGroupRoutes);
+app.use("/api/addon-options", addonOptionRoutes);
+app.use("/api/vouchers", voucherRoutes);
 
 // 404
 app.use((req, res) => {
