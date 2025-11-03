064804c70379c6497a6a6494954635b79dbc4c5e
Nguyen-Thanh-Nam <thanhnam3102004@gmail.com>
2025-11-03 16:39:10 +0700

    create about

---
+++ client/src/app/about/page.jsx
@@ -0,0 +1,11 @@
+import Banner from "./Banner";
+import OurStory from "./OurStory";
+
+export default function Home() {
+  return (
+    <main>
+      <Banner />
+      <OurStory />
+    </main>
+  );
+}
