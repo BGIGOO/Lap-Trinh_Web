c4d53f85c2098be5c99f8e1a1f3ef65ec09b6705
Nguyen-Thanh-Nam <thanhnam3102004@gmail.com>
2025-11-06 11:08:51 +0700

    update menu section

--- client/src/app/about/Menu_Section.jsx
+++ client/src/app/about/Menu_Section.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function MenuSection() {
   return (
@@ -127,6 +128,68 @@ export default function MenuSection() {
               priority
             />
           </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center mt-10 mb-16">
+        <Link
+          href="/product"
+          className="bg-[#FC4126] text-white font-extrabold uppercase text-sm py-3 px-8 rounded-full hover:bg-[#e2371f] transition-all duration-300 shadow-md hover:shadow-lg"
+        >
+          Xem sản phẩm
+        </Link>
+      </div>
+
+      <div className="flex justify-center gap-20 overflow-x-auto px-6 pb-10">
+        {/* Ảnh 1 */}
+        <div className="relative w-[241px] h-[366px] flex-shrink-0 rounded-2xl overflow-hidden">
+          <Image
+            src="/Menu_Section/MenuLine_1.png"
+            alt="Menu Line 1"
+            fill
+            className="object-cover"
+            priority
+          />
+        </div>
+
+        {/* Ảnh 2 */}
+        <div className="relative w-[229px] h-[272px] flex-shrink-0 rounded-2xl overflow-hidden">
+          <Image
+            src="/Menu_Section/MenuLine_2.png"
+            alt="Menu Line 2"
+            fill
+            className="object-cover "
+          />
+        </div>
+
+        {/* Ảnh 3 */}
+        <div className="relative w-[230px] h-[366px] flex-shrink-0 rounded-2xl overflow-hidden">
+          <Image
+            src="/Menu_Section/MenuLine_3.png"
+            alt="Menu Line 3"
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        {/* Ảnh 4 */}
+        <div className="relative w-[230px] h-[272px] flex-shrink-0 rounded-2xl overflow-hidden">
+          <Image
+            src="/Menu_Section/MenuLine_4.png"
+            alt="Menu Line 4"
+            fill
+            className="object-cover rounded-2xl"
+          />
+        </div>
+
+        {/* Ảnh 5 */}
+        <div className="relative w-[230px] h-[366px] flex-shrink-0 rounded-2xl overflow-hidden">
+          <Image
+            src="/Menu_Section/MenuLine_5.png"
+            alt="Menu Line 5"
+            fill
+            className="object-cover rounded-2xl"
+          />
         </div>
       </div>
     </section>
