009d84899b6b2f660f62b03b9fb30a494477f4e7
Nguyen-Thanh-Nam <thanhnam3102004@gmail.com>
2025-11-12 00:19:19 +0700

    city line

--- client/src/components/CityLines.jsx
+++ client/src/components/CityLines.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Cityline() {
   return (
-    <div className="relative w-full h-[224] flex justify-center bg-white overflow-hidden pb-4">
+    <div className="relative w-full h-[223] flex justify-center bg-white overflow-hidden">
       <Image src="/cityline/full.png" alt="City line" fill priority />
     </div>
   );
