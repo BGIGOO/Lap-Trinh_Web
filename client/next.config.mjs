1347b0f142cbef5c84652ccb5280a23d10d747c3
toilaminhbui <toilaminhbui@gmail.com>
2025-11-19 17:09:37 +0700

    change config and add  lib

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3001",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "3001",
+                pathname: "/uploads/**",
+            },
+            {
+                protocol: "https",
+                hostname: "qr.sepay.vn",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
